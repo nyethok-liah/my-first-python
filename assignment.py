970637cfbea0383997e087411175508b8870f5e8
Nyethok Liah <nyethokliah@gmail.com>
2026-09-16 12:03:49 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,11 @@
+# my first project in python
+name = "Nyapieth Thor"
+age = 20
+my_favorite_language = "python"
+my_goal = "to become a software engineer"
+introduction ="Hello, my name is Nyapieth Thor. I am 20 years old. My favorite programming language is python. my goal is to become a software engineer."
+print(name)
+print(age)
+print(my_favorite_language)
+print(my_goal)
+print(introduction)
